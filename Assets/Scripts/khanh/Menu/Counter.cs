c0746b77d466fffd16060553106950e0a015b3db
Dat Huynh <dat.huynh.ext@karlstorz.com>
2022-01-28 06:54:48 +0100

    Add counter component

---
+++ Assets/Scripts/khanh/Menu/Counter.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField]
+    public int MaxValue;
+
+    [SerializeField]
+    public int MinValue;
+
+    [SerializeField]
+    public int Value {
+        get
+        {
+            return _value;
+        } 
+        set {
+            _value = value;
+            ValueText.text = _value.ToString();
+        } 
+    }
+
+    private int _value; 
+    public Button IncrementButton;
+    public Button DecrementButton;
+    public Button ValueDisplay;
+    private Text ValueText;
+    public UnityEvent OnValueChanged;
+
+    private void Increment()
+    {
+        if (Value >= MaxValue) return;
+        Value++;
+        OnValueChanged.Invoke();
+    }
+
+    private void Decrement()
+    {
+        if (Value <= MinValue) return;
+        Value--;
+        OnValueChanged.Invoke();
+    }
+
+    private void Awake()
+    {
+        ValueDisplay.enabled = false;
+        ValueText = ValueDisplay.GetComponentInChildren<Text>();
+        Value = MinValue;
+        IncrementButton.onClick.AddListener(Increment);
+        DecrementButton.onClick.AddListener(Decrement);
+    }
+}
